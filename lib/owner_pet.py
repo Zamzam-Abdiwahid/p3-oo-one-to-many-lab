110d345dc49330667fe5f51979d6b70ea9686b74
Zamzam Abdiwahid <zamzamabdiwahid123@gmail.com>
2024-07-27 16:05:55 +0300

    Creating a one-to-many relationship

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,30 @@
 class Pet:
-    pass
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+    all = []
+
+    def __init__(self, name, pet_type, owner=None):
+        if pet_type not in self.PET_TYPES:
+            raise Exception("Invalid pet type")
+        self.name = name
+        self.pet_type = pet_type
+        if owner:
+            owner.add_pet(self)
+        self.owner = owner
+        self.all.append(self)
 
 class Owner:
-    pass
+    def __init__(self, name):
+        self.name = name
+        self._pets = []
+
+    def pets(self):
+        return self._pets
+
+    def add_pet(self, pet):
+        if not isinstance(pet, Pet):
+            raise Exception("Invalid pet type")
+        self._pets.append(pet)
+        pet.owner = self
+
+    def get_sorted_pets(self):
+        return sorted(self._pets, key=lambda pet: pet.name)
